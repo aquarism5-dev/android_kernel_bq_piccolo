417b4591b8bf8cc81b772fe0a64baba6e5879943
darknightmare <rubentkd98@gmail.com>
2015-08-10 02:06:30 +0200

    prima: Updates for piccolo

--- drivers/staging/prima/Android.mk
+++ drivers/staging/prima/Android.mk
@@ -104,11 +104,11 @@ LOCAL_MODULE_PATH         := $(TARGET_OUT)/lib/modules/$(WLAN_CHIPSET)
 include $(DLKM_DIR)/AndroidKernelModule.mk
 ###########################################################
 
-#Create symbolic link
+# Create symbolic link
 $(shell mkdir -p $(TARGET_OUT)/lib/modules; \
         ln -sf /system/lib/modules/$(WLAN_CHIPSET)/$(WLAN_CHIPSET)_wlan.ko \
                $(TARGET_OUT)/lib/modules/wlan.ko)
 
 endif # DLKM check
 
-endif # supported target check
+endif # Supported target check
